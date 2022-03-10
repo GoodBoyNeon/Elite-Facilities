588085d06dba607382b73c0ee840691191dbe318
GoodBoyNeon <neonfavor160@gmail.com>
2022-03-10 20:08:13 +0545

    FIXES

--- src/slashcommands/Util/avatar.js
+++ src/slashcommands/Util/avatar.js
@@ -1,32 +1,37 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
-const run = async (client, interaction) => {
-	let target = interaction.options.getMember('user') || interaction.member
-
-	try {
-		const avatarEmbed = new MessageEmbed()
-		.setTitle(`${target.user.username}\'s avatar`)
-		.setImage(target.displayAvatarURL({ size: 4096, dynamic: true }))
-		.setColor('WHITE')
-		.setFooter(`Requested by ${interaction.member.user.tag}`, interaction.member.displayAvatarURL({ dynamic: true }))
-
-		interaction.reply({ embeds: [avatarEmbed] });
-	} catch(err) {
-		if(err) {
-			console.error(err);
-		}
-	}
-}
+const run = async (client, interaction) => {};
 
 module.exports = {
-	name: 'avatar',
-	description: 'get a member\'s or your own avatar',
-	options: [
-		{
-			name: 'user',
-			description: 'mention the user who\'s avatar you want, leave this field empty if you want your own avatar.',
-			type: 'USER',
-			required: false
-		}
-	], run
-}
+  name: "avatar",
+  description: "get a member's or your own avatar",
+  options: [
+    {
+      name: "user",
+      description:
+        "mention the user who's avatar you want, leave this field empty if you want your own avatar.",
+      type: "USER",
+      required: false,
+    },
+  ],
+  run: async (client, interaction) => {
+    let target = interaction.options.getMember("user") || interaction.member;
+
+    try {
+      const avatarEmbed = new MessageEmbed()
+        .setTitle(`${target.user.username}\'s avatar`)
+        .setImage(target.displayAvatarURL({ size: 4096, dynamic: true }))
+        .setColor("WHITE")
+        .setFooter({
+          text: `Requested by ${interaction.member.user.tag}`,
+          iconURL: `${interaction.member.displayAvatarURL({ dynamic: true })}`,
+        });
+
+      interaction.reply({ embeds: [avatarEmbed] });
+    } catch (err) {
+      if (err) {
+        console.error(err);
+      }
+    }
+  },
+};
--- src/slashcommands/Util/serverinfo.js
+++ src/slashcommands/Util/serverinfo.js
@@ -1,38 +1,101 @@
-const { MessageEmbed } = require('discord.js')
-
-const run = (client, interaction) => {
-	try {
-		let serverinfoEmbed = new MessageEmbed()
-      .setTitle("Server Information")
-      .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
-      .setFooter({ text: `Requested by ${interaction.member.user.tag}`, iconURL: `${interaction.member.displayAvatarURL()}` })
-      .setTimestamp()
-      .setAuthor({ name: `${interaction.guild.name}`, iconURL: `${interaction.guild.iconURL()}`})
-      .setColor("BLURPLE")
-      .addFields(
-        { name: 'Server Name', value: `${interaction.guild.name}`, inline: true },
-        { name: 'Server ID', value: `${interaction.guild.id}`, inline: true },
-        { name: 'Server Owner', value: `<@${interaction.guild.ownerId}>`, inline: true },
-        { name: 'Total Members', value: `${interaction.guild.members.cache.size}`, inline: true },
-        { name: 'Total Bots', value: `${interaction.guild.members.cache.filter(member => member.user.bot).size}`, inline: true },
-        { name: 'Total Emojis', value: `${interaction.guild.emojis.cache.size}`, inline: true },
-        { name: 'Animated Emojis', value: `${interaction.guild.emojis.cache.filter(emoji => emoji.animated).size}`, inline: true },
-        { name: 'Total Text Channels', value: `${interaction.guild.channels.cache.filter(channel => channel.type === 'text').size}`, inline: true },
-        { name: 'Total Voice Channels', value: `${interaction.guild.channels.cache.filter(channel => channel.type === 'voice').size}`, inline: true },
-        { name: 'Created At', value: `${interaction.guild.createdAt.toDateString()}`, inline: true },
-        { name: 'Total Roles', value: `${interaction.guild.roles.cache.size}`, inline: true },
-        { name: 'Total Boosters', value: `${interaction.guild.premiumSubscriptionCount}`, inline: true },
-      )
-     interaction.reply({ embeds: [serverinfoEmbed]});
-	} catch(err) {
-		if(err) {
-			console.error(err);
-		}
-	}
-}
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'serverinfo',
-	description: 'gives information about the server',
-	run
-}
+  name: "serverinfo",
+  description: "gives information about the server",
+  run: async (client, interaction) => {
+    try {
+      let serverinfoEmbed = new MessageEmbed()
+        .setTitle("Server Information")
+        .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
+        .setFooter({
+          text: `Requested by ${interaction.member.user.tag}`,
+          iconURL: `${interaction.member.displayAvatarURL()}`,
+        })
+        .setTimestamp()
+        .setAuthor({
+          name: `${interaction.guild.name}`,
+          iconURL: `${interaction.guild.iconURL()}`,
+        })
+        .setColor("BLURPLE")
+        .addFields(
+          {
+            name: "Server Name",
+            value: `${interaction.guild.name}`,
+            inline: true,
+          },
+          { name: "Server ID", value: `${interaction.guild.id}`, inline: true },
+          {
+            name: "Server Owner",
+            value: `<@${interaction.guild.ownerId}>`,
+            inline: true,
+          },
+          {
+            name: "Total Members",
+            value: `${interaction.guild.members.cache.size}`,
+            inline: true,
+          },
+          {
+            name: "Total Bots",
+            value: `${
+              interaction.guild.members.cache.filter(
+                (member) => member.user.bot
+              ).size
+            }`,
+            inline: true,
+          },
+          {
+            name: "Total Emojis",
+            value: `${interaction.guild.emojis.cache.size}`,
+            inline: true,
+          },
+          {
+            name: "Animated Emojis",
+            value: `${
+              interaction.guild.emojis.cache.filter((emoji) => emoji.animated)
+                .size
+            }`,
+            inline: true,
+          },
+          {
+            name: "Total Text Channels",
+            value: `${
+              interaction.guild.channels.cache.filter(
+                (channel) => channel.type === "text"
+              ).size
+            }`,
+            inline: true,
+          },
+          {
+            name: "Total Voice Channels",
+            value: `${
+              interaction.guild.channels.cache.filter(
+                (channel) => channel.type === "voice"
+              ).size
+            }`,
+            inline: true,
+          },
+          {
+            name: "Created At",
+            value: `${interaction.guild.createdAt.toDateString()}`,
+            inline: true,
+          },
+          {
+            name: "Total Roles",
+            value: `${interaction.guild.roles.cache.size}`,
+            inline: true,
+          },
+          {
+            name: "Total Boosters",
+            value: `${interaction.guild.premiumSubscriptionCount}`,
+            inline: true,
+          }
+        );
+      interaction.reply({ embeds: [serverinfoEmbed] });
+    } catch (err) {
+      if (err) {
+        console.error(err);
+      }
+    }
+  },
+};
